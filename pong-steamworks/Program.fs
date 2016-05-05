b15336dc519e9446fe8940e657c78ec3ac5186d1
alann <alann@DESKTOP-95N4BVT>
2016-05-05 00:08:21 -0700

    First commit

--- pong-steamworks/Program.fs
+++ pong-steamworks/Program.fs
@@ -1,7 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open PongGame
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    use g = new Game1()
+    g.Run()
+    printfn "Hello World"
     0 // return an integer exit code
