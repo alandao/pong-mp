f345d217078c2a8b9320394ce74e84076b60f790
alann <alann@DESKTOP-95N4BVT>
2016-05-24 07:18:51 -0700

    using lidgren instead of steamworks for now

--- pong-steamworks/Program.fs
+++ pong-steamworks/Program.fs
@@ -1,17 +1,7 @@
 ﻿open PongClient
-open Steamworks
 
 [<EntryPoint>]
 let main argv =
-    let steamAPIInit = SteamAPI.Init()
-    if not steamAPIInit then
-        printfn "Error: SteamAPI_Init() failed"
-        System.Environment.Exit 1
-
-    let loggedIn = SteamUser.BLoggedOn()
-    if not loggedIn then
-        printfn "Error: Steam user isn't logged in"
-        System.Environment.Exit 1
 
     //initialize client
     use client = new PongClient()
@@ -19,5 +9,4 @@ let main argv =
     client.Run()
 
     printfn "Shutting Down"
-    SteamAPI.Shutdown()
     0 // return an integer exit code
