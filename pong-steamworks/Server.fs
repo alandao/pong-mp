804b6875f94994b741de2576e3a08dd3366aa20c
alann <alann@DESKTOP-F1O55GM>
2016-09-03 19:34:15 -0700

    refactored server code into Server.fs

---
+++ pong-steamworks/Server.fs
@@ -0,0 +1,46 @@
+﻿module Server
+
+open Lidgren.Network
+
+open ECS
+open ECSTypes
+open NetBuffer
+
+
+let StartSocket port =
+    let config = new NetPeerConfiguration("pong")
+    config.Port <- port
+    let server = new NetServer(config)
+    server.Start()
+    server
+
+let ProcessClientMessages currentClients (serverSocket:NetServer) =
+    let mutable newClients:NetConnection list = []
+
+    //process messages from clients
+    let mutable message = serverSocket.ReadMessage()
+    while message <> null do
+        match message.MessageType with
+        | NetIncomingMessageType.Data ->
+            let data = message.ReadString()
+            printfn "Server: message '%s' received" data
+        
+        | NetIncomingMessageType.StatusChanged ->
+            //A client connected or disconnected.
+            match message.SenderConnection.Status with
+            | NetConnectionStatus.Connected ->
+                newClients <- message.SenderConnection::newClients
+                printfn "Server: Client from %s has connected." (message.SenderEndPoint.ToString())
+            | NetConnectionStatus.Disconnected ->
+                //remove client from connections list
+                //clients' <- List.filter (fun x -> x <> message.SenderConnection) clients'
+                printfn "Server: Client from %s has disconnected." (message.SenderEndPoint.ToString())
+            | _ ->
+                printfn "Server: Client from %s has an unhandled status." (message.SenderEndPoint.ToString())
+           
+        | NetIncomingMessageType.DebugMessage -> 
+            ()
+        | _ ->
+            eprintfn "Server: Unhandled message with type: %s" (message.MessageType.ToString())
+            ()
+        message <- serverSocket.ReadMessage()
