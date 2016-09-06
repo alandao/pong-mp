09ec3d6db6691a842892a9f48e9b11999af0928d
alann <alann@DESKTOP-F1O55GM>
2016-09-05 21:54:09 -0700

    added RunFrame func

--- pong-steamworks/Client.fs
+++ pong-steamworks/Client.fs
@@ -9,67 +9,27 @@ open System.IO
 open Lidgren.Network
 
 open HelperFunctions
-open SharedServerClient
 
-
-
-type World = {
-    entities : HashSet<Entity>;
-
-    position: Dictionary<Entity, Position>;
-    velocity: Dictionary<Entity, Velocity>;
-    appearance: Dictionary<Entity, Appearance>;
-    }
-
-let defaultWorld = {
-    entities = HashSet<Entity>();
-    position = Dictionary<Entity, Position>();
-    velocity = Dictionary<Entity, Velocity>();
-    appearance = Dictionary<Entity, Appearance>();
-    }
-
-let DestroyEntity id world = 
-    world.position.Remove(id) |> ignore
-    world.velocity.Remove(id) |> ignore
-    world.entities.Remove(id)
-
-let AddAppearance id textureName (world:World)  = 
-    let appr =  {    
-                    texture = textureName; 
-                    size = Vector2(1.f, 1.f);
-                }
-    world.appearance.Add(id, appr)
 
 //  SYSTEMS
 
 //draw entities with position and appearance
-let RunAppearance textures (sb:SpriteBatch) dummyTexture world =
-    for entry in world.appearance do
-        let id = entry.Key
-        let appearance = entry.Value
-
-        let position = tryFind id world.position
-        let texture = tryFind appearance.texture textures
-
-        if Option.isNone position then
-            eprintfn "Client.RunAppearance: Cannot render entity \"%s\" without a position component!" id
-        else            
-            if Option.isNone texture then
-                eprintfn "Client.RunAppearance: Texture missing for \"%s\"!" id
-                sb.Draw(dummyTexture, Option.get position, Color.Magenta)
-            else
-                sb.Draw(Option.get texture, Option.get position, Color.White)
-
-
-//  OTHER
-
-let (GetClientInputs : PlayerInput list) =
-    let mutable inputs = List.empty
-    if (Keyboard.GetState().IsKeyDown(Keys.W)) then
-        inputs <- PaddleUp true :: inputs
-    if (Keyboard.GetState().IsKeyDown(Keys.S)) then
-        inputs <- PaddleDown true :: inputs
-    inputs
+//let RunAppearance textures (sb:SpriteBatch) dummyTexture world =
+//    for entry in world.appearance do
+//        let id = entry.Key
+//        let appearance = entry.Value
+//
+//        let position = tryFind id world.position
+//        let texture = tryFind appearance.texture textures
+//
+//        if Option.isNone position then
+//            eprintfn "Client.RunAppearance: Cannot render entity \"%s\" without a position component!" id
+//        else            
+//            if Option.isNone texture then
+//                eprintfn "Client.RunAppearance: Texture missing for \"%s\"!" id
+//                sb.Draw(dummyTexture, Option.get position, Color.Magenta)
+//            else
+//                sb.Draw(Option.get texture, Option.get position, Color.White)
 
 
 //Public facing functions
@@ -80,24 +40,17 @@ let StartSocket (ip:string) port =
     ignore(client.Connect(ip, port))
     client
 
-let Update (world:World) (clientSocket:NetClient) =
-    let mutable message = clientSocket.ReadMessage()
+let RunFrame serverState (socket : NetClient) dt =
+    let mutable message = socket.ReadMessage()
 
     while message <> null do
         match message.MessageType with
         | NetIncomingMessageType.Data ->
             printfn "Client: Incoming packet byte size: %i" message.LengthBytes
-            let sizeSharedEntities = message.ReadInt32()
-            printfn "Client: SharedEntities size: %i" sizeSharedEntities
-            for i in 0..sizeSharedEntities - 1 do
-                let entity = message.ReadString()
-                world.entities.Add(entity)
-            let ackMsg = message.ReadString()
-            printfn "Client: Ack msg: %s" ackMsg
 
         | NetIncomingMessageType.StatusChanged -> ()
         | NetIncomingMessageType.DebugMessage -> ()
         | _ ->
             eprintfn "Client: Unhandled message with type: %s" (message.MessageType.ToString())
             ()
-        message <- clientSocket.ReadMessage()
+        message <- socket.ReadMessage()
